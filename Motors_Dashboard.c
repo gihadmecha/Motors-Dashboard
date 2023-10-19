df5f56178b6e5f5aad6f3dec2abb3c4a1e803e00
gihadmecha <gihadmecha@gmail.com>
2023-10-19 08:36:08 +0200

    control DC Motor Speed & control Servo

--- Motors_Dashboard.c
+++ Motors_Dashboard.c
@@ -7,13 +7,20 @@ extern void Motors_Dashboard_Init ()
 	DIO_Init();
 	ADC_Init(ADC_VCC, ADC_PRESCALER_8);
 	ADC_Enable();
+	TIMER0_Init(TIMER0_PRESCALER_8, TIMER0_PHASE_CORRECT_PWM, TIMER0_OC0_SET_ON_COMPARE_MATCH);
 	TIMER1_Init(TIMER1_PRESCALER_8, TIMER1_PHASE_CORRECT_PWM_ICR1_TOP, TIMER1_OC1A_SET_ON_COMPARE_MATCH, TIMER1_OC1B_SET_ON_COMPARE_MATCH);
-	MOTOR1_Init();
 	LCD_Init();
 }
 
 extern void Motors_Dashboard_Run ()
 {
 	MOTOR1_Forward(POTONTIOMETER1 ());
-	SERVO_setAngle (POTONTIOMETER2 ());
+	
+	double servoAngle = (POTONTIOMETER2() * 180.0) / 100.0 - 90.0;
+	LCD_GoTo(0, 0);
+	LCD_WriteNumber_4Digit(POTONTIOMETER2());
+	LCD_GoTo(1, 0);
+	LCD_WriteNumber_4Digit((u16)servoAngle);
+	SERVO_setAngle (servoAngle);
+	
 }
